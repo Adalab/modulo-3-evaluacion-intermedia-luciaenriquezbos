c53bc7c6e4c468f1dcbc5814f1b4c38e03586ca6
luciaenriquezbos <eboscreative@gmail.com>
2021-06-08 10:47:31 +0200

    add key

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -2,25 +2,10 @@ import React from "react";
 import Pokemon from "./Pokemon";
 import "../stylesheets/App.css";
 
-// const PokeList = (props) => {
-//   const pokeItems = props.list.map( (item, index) => {
-//     console.log(pokeItems);
-
-//     return (
-//       <li key={index}>
-//       <Pokemon item={item} />
-//       </li>
-//   );
-//   )
-//  };
-
-// // return <ul>{pokeItems}</ul>;
-// };
-
 const PokeList = (props) => {
-  const pokeItems = props.list.map((item) => {
+  const pokeItems = props.list.map((item, index) => {
     return (
-      <li>
+      <li key={index}>
         <Pokemon item={item} />
       </li>
     );
